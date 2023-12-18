88b7bf31a703798eb1f7ec79b6f465cf017d8175
Mdp88 <matiaspanguilto@hotmail.com>
2023-12-18 17:20:03 -0300

    Init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,102 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import "./index.css";
+import { useEffect, useRef, useState } from "react";
+import { IoHandLeftOutline } from "react-icons/io5";
 
 function App() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [section, setSection] = useState(0);
+
+  const handleScroll = (e) => {
+    // console.log(e);
+    const position = e.target.scrollTop;
+    const h =
+      (100 / (e.target.scrollHeight - e.target.offsetHeight)) * position;
+    // console.log(h);
+    setScrollPosition(Math.round(h));
+    console.log(Math.round(h));
+  };
+
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <div className="container" onScroll={handleScroll}>
+      <div className="canvasContainer">
+        <Canvas
+          shadows
+          camera={{ position: [1, 2.7, 4.7], fov: 30 }}
+          color="black"
+        >
+          <color attach="background" args={["#ececec"]} />
+          <Experience
+            position={scrollPosition}
+            section={section}
+            setSection={setSection}
+          />
+        </Canvas>
+      </div>
+      <div className="content">
+        <section>
+          <div
+            className="section-content hello"
+            style={{
+              opacity: `${100 - scrollPosition * 8}%`,
+            }}
+          >
+            <h1>
+              <span id="wave">✋🏼</span>
+              Hey There!
+            </h1>
+            <h2>Welcome to my Portfolio...</h2>
+            <h3>
+              I'm <span id="name">Matías Panguilto</span>, a software developer
+              who loves crafting visually engaging, interactive, and modern UIs.
+              I specialize in transforming concepts into polished, user-friendly
+              websites. With a proven track record of delivering innovative
+              solutions and a passion for staying at the forefront of industry
+              trends, I am confident in my ability to contribute effectively to
+              your team.
+            </h3>
+          </div>
+        </section>
+        <section>
+          <div
+            className="section-content experience"
+            style={{
+              opacity:
+                scrollPosition >= 33
+                  ? `${100 - (scrollPosition - 33) * 8}%`
+                  : `${100 - (33 - scrollPosition) * 8}%`,
+            }}
+          >
+            <h1>
+              <span id="experience">📝</span>Experience
+            </h1>
+            <h3>
+              I'm <span id="name">Matías Panguilto</span>, a software developer
+              with a passion for crafting seamless digital experiences that
+              blend elegance with functionality. I specialize in turning
+              concepts into polished, user-friendly websites.
+            </h3>
+          </div>
+        </section>
+        <section>
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            dicta deleniti temporibus, corporis officiis commodi quas pariatur
+            molestiae inventore quisquam eveniet impedit numquam perferendis
+            sapiente sed nulla vel facere quasi!
+          </h1>
+        </section>
+        <section>
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+            dicta deleniti temporibus, corporis officiis commodi quas pariatur
+            molestiae inventore quisquam eveniet impedit numquam perferendis
+            sapiente sed nulla vel facere quasi!
+          </h1>
+        </section>
+      </div>
+    </div>
   );
 }
 
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,202 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+  Sky,
+  Sphere,
+  useTexture,
+  Icosahedron,
+} from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useControls } from "leva";
+import { Chair } from "./Chair";
+import { Desk } from "./Desk";
+import { useEffect, useRef, useState, useCallback } from "react";
+import { MathUtils } from "three";
+import { useFrame, useThree, useLoader } from "@react-three/fiber";
+import gsap from "gsap";
+import { debounce } from "lodash";
+import * as THREE from "three";
+import { TextureLoader } from "three/src/loaders/TextureLoader";
 
-export const Experience = () => {
+export const Experience = ({ position, section, setSection }) => {
+  const { camera, intersect } = useThree();
+  const group = useRef();
+  const sky = useRef();
+  const wire = useRef();
+
+  const colorMap = useTexture("assets/seamlessNight.jpg");
+  colorMap.wrapS = colorMap.wrapT = THREE.RepeatWrapping;
+
+  colorMap.repeat.set(5, 5);
+
+  const [animation, setAnimation] = useState("Idle");
+  const [follow, setFollow] = useState(true);
+
+  useEffect(() => {
+    camera.position.y = 2;
+    camera.position.z = 2.3;
+    camera.lookAt(0, 1.3, 2);
+  }, []);
+
+  useEffect(() => {
+    if (position <= 16.5) {
+      debouncedSection(0);
+    } else if (position > 16.5 && position <= 50) {
+      debouncedSection(1);
+    } else if (position > 50 && position <= 83.5) {
+      debouncedSection(2);
+    } else {
+      debouncedSection(3);
+    }
+  }, [position]);
+
+  useEffect(() => {
+    console.log(section);
+    switch (section) {
+      case 0:
+        setAnimation("Idle");
+        gsap.to(camera.position, {
+          y: 2,
+          x: 1,
+          z: 2.3,
+          duration: 1,
+          onUpdate: () => {
+            camera.lookAt(0, 1.3, 0);
+          },
+        });
+        gsap.to(group.current.rotation, {
+          y: MathUtils.degToRad(0),
+          duration: 3,
+          onComplete: () => {
+            setFollow(true);
+          },
+        });
+        gsap.to(group.current.position, {
+          y: 0,
+          x: 0.7,
+          duration: 3,
+        });
+
+        break;
+      case 1:
+        setAnimation("Typing");
+        setFollow(false);
+        gsap.to(camera.position, {
+          z: 1,
+          x: 2,
+          y: 1.3,
+          duration: 1,
+          onUpdate: () => {
+            camera.lookAt(0, 1.3, 0);
+          },
+        });
+        gsap.to(group.current.rotation, {
+          y: MathUtils.degToRad(0),
+          duration: 3,
+        });
+        gsap.to(group.current.position, {
+          y: 0.3,
+          x: 1,
+          duration: 3,
+        });
+
+        break;
+      case 2:
+        setAnimation("Typing");
+        setFollow(false);
+
+        gsap.to(camera.position, {
+          z: 6,
+          x: 0,
+          duration: 3,
+          onUpdate: () => {
+            camera.lookAt(0, 1.3, 0);
+          },
+        });
+        gsap.to(group.current.rotation, {
+          y: MathUtils.degToRad(0),
+          duration: 1,
+        });
+        gsap.to(group.current.position, {
+          y: 0.3,
+          x: 0,
+          duration: 3,
+        });
+
+        break;
+      case 3:
+        setAnimation("Talking");
+        setFollow(false);
+
+        gsap.to(camera.position, {
+          z: 1.5,
+          duration: 3,
+          onUpdate: () => {
+            camera.lookAt(0, 1.3, 0);
+          },
+        });
+        gsap.to(group.current.rotation, {
+          y: MathUtils.degToRad(0),
+          duration: 1,
+          onComplete: () => {
+            setFollow(true);
+          },
+        });
+        gsap.to(group.current.position, {
+          y: 0,
+          x: 0,
+          duration: 1,
+        });
+
+        break;
+
+      default:
+        setAnimation("Idle");
+        setFollow(false);
+
+        gsap.to(camera.position, {
+          y: 2,
+          z: 2.3,
+          duration: 3,
+          onUpdate: () => {
+            camera.lookAt(0, 1.3, 0);
+          },
+        });
+        break;
+    }
+  }, [section]);
+
+  const debouncedSection = useCallback((value) => changeSection(value), []);
+
+  const changeSection = debounce((value) => {
+    setSection(value);
+  }, 500);
+
+  useFrame(() => {
+    // sky.current.rotation.y += 0.0002;
+    wire.current.rotation.y -= 0.0001;
+    wire.current.rotation.z += 0.0002;
+  });
+
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+      <PerspectiveCamera />
+      <Environment preset="apartment" />
+      <Sphere ref={sky} scale={[30, 30, 30]}>
+        <meshStandardMaterial side={THREE.BackSide} color="black" />
+      </Sphere>
+      {/* <Sphere ref={wire} scale={[29, 29, 29]}> */}
+      <mesh ref={wire} scale={[29, 29, 29]}>
+        <icosahedronGeometry args={[1, 5]} />
+        <meshStandardMaterial wireframe side={THREE.BackSide} color="#1E90FF" />
       </mesh>
+      {/* </Sphere> */}
+      <group ref={group}>
+        <Avatar animation={animation} follow={follow} />
+        <Chair animation={animation} />
+        <Desk animation={animation} />
+      </group>
     </>
   );
 };
